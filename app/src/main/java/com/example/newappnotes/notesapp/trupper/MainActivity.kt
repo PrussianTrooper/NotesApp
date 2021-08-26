4e0a1f0b09ba0262e78310d0e70a957c881959c5
Lukoil39 <goshavasilevskiy@mail.ru>
2021-08-26 16:04:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/newappnotes/notesapp/trupper/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.newappnotes.notesapp.trupper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
